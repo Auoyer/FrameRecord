1fe193a676ada6a9f10ac1e4fd3c6d4a4afc3a9b
huangzhongqing <huangzhongqing@vcredit.com>
2019-03-28 18:54:16 +0800

    提交

---
+++ QuartzProject/QuartzSrvWithDB/Jobs/DoSomething.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace QuartzSrvWithDB.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class DoSomething : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            File.WriteAllText(@"E:\QuartzSrvWithDB.txt", "3.0");
+            return Task.FromResult(true);
+        }
+    }
+}
