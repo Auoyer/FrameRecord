9595287604390aa2dd9bb05f81569f387e585e8d
huangzhongqing <huangzhongqing@vcredit.com>
2019-09-03 09:16:33 +0800

    update

---
+++ QuartzProject/Quartz_Core/App_Start/JobScheduler.cs
@@ -0,0 +1,50 @@
+﻿using NLog;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Simpl;
+using Quartz.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quartz_Core.App_Start
+{
+    public class JobScheduler
+    {
+        public static IScheduler scheduler;
+        public static Logger Logger = LogManager.GetCurrentClassLogger();
+        static JobScheduler()
+        {
+            try
+            {
+                XMLSchedulingDataProcessor processor = new XMLSchedulingDataProcessor(new SimpleTypeLoadHelper());
+                StdSchedulerFactory factory = new StdSchedulerFactory();
+                scheduler = factory.GetScheduler().GetAwaiter().GetResult();
+                processor.ProcessFileAndScheduleJobs("~/quartz_jobs.xml", scheduler);
+            }
+            catch (System.Exception ex)
+            {
+                Logger.Error(ex, $"任务加载异常。");
+            }
+        }
+
+        public static void Start()
+        {
+            if (scheduler != null)
+            {
+                scheduler.Start();
+                Logger.Info($"任务开启。");
+            }
+        }
+
+        public static void Stop()
+        {
+            if (scheduler != null)
+            {
+                scheduler.Shutdown();
+                Logger.Info($"任务停止。");
+            }
+        }
+    }
+}
---
+++ QuartzProject/Quartz_Core/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using Quartz_Core.App_Start;
+
+namespace Quartz_Core
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // 注册Quartz定时任务
+            JobScheduler.Start();
+            // 注册Nlog
+            loggerFactory.AddNLog();
+            env.ConfigureNLog("nlog.config");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
